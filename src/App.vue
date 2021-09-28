672a8412284afb747a0e580cce6e19b640b7d1f8
Gabrielecorti01 <gcorti.info@gmail.com>
2021-09-28 16:09:08 +0200

    Esercizio sviluppo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
+
+    <ul>
+      <li v-for="(element, index) in numeroRandomico" :key="index">
+        {{ numeroRandomico }}
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+data: function(){
+  return {
+    numeroRandomico : '',
   }
+},
+
+  mounted() {
+    console.log(axios);
+
+    axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+
+  .then ((response) => {
+    const result = response.data;
+    console.log(result.response);
+    this.numeroRandomico = result.response;
+    });
+  } 
 }
+
 </script>
 
 <style lang="scss">
